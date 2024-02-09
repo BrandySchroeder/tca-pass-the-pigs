12b3825b44809415e1712a6a4f2965f80d592dbf
Brandy Schroeder <brandyschroeder510@gmail.com>
2024-02-09 13:43:42 -0600

    Proper app nav
    it's all connected now :)

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+    
 export const Home = () => {
+
+    //Hooks and State at the top
+    const nav = useNavigate();
+
     return (
         <>
             <h3>
                 Home
             </h3>
             <button 
-                className="btn btn-secondary">
+                className="btn btn-secondary"
+                onClick={() => nav('/setup')}
+                >
                     Play
                 </button>
         </>
--- src/Play.tsx
+++ src/Play.tsx
@@ -1,17 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 export const Play = () => {
+
+    const nav = useNavigate();
+
     return (
-    <>
-    <h3>
-        Play
-    </h3>
-    <p>
-        Play the game and tap the app!!
-    </p>
-    <button
-        className="btn btn-outline btn-primary"
-        >
-        Done
-    </button>
-    </>
+        <>
+            <h3>
+                Play
+            </h3>
+            <p>
+                Play the game and tap the app!!
+            </p>
+            <button
+                className="btn btn-outline btn-primary"
+                onClick={() => nav(-2)}
+            >
+                Done
+            </button>
+        </>
     );
   };
