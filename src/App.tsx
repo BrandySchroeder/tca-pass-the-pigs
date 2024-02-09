2d44561d3af8a500ff789c290b96f912084f725f
Brandy Schroeder <brandyschroeder510@gmail.com>
2024-02-09 10:56:28 -0600

    a card componoent
    UI - hammer
    p-3 for app padding
    
    no w-96 to keep width down on mobile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App p-3">
       <button className="btn btn-primary">Hello daisyUI</button>
+      <div className="card bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
