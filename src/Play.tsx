b2220bb926b8c54a5bd9e43dc47dc767066ef43b
Brandy Schroeder <brandyschroeder510@gmail.com>
2024-04-12 15:05:33 -0500

    All point buttons working except Pig out and Oinker!
    Need to hook up functionality for those 2 buttons next. Add a "next player" button to indicate when one's turn is done?

--- src/Play.tsx
+++ src/Play.tsx
@@ -18,6 +18,7 @@ export const Play: FC<PlayProps> = ({
     , chosenPlayers 
 }) => {
 
+    //ASK: why is setStart dim, declared but not read?
     const [start, setStart] = useState(new Date().toISOString());
 
 //Trying Tom's code for point counter buttons - this should keep track of each player's points separately in their own cards
@@ -61,11 +62,19 @@ const gameOver = (winner: string) => {
                         </h2>
                         <h2
                             className='text-4xl font-bold mx-5 min-w-10 text-left'
-                        >
+                        >This Game: 
                             {
                                 playerPoints.find(y => y[0] === x)![1]
                             }
                         </h2>
+                        <h2
+                            className='text-2xl font-bold mx-5 min-w-10 text-left'
+                        >This Turn: *Placeholder* Need to hook up functionality!
+                            {
+                                playerPoints.find(y => y[0] === x)![1]
+                            }
+                        </h2>
+
                             <div className="flex gap-3 items-center mb-5"
                             >
                                 <h3>Sider: 1 point each</h3>
@@ -79,7 +88,6 @@ const gameOver = (winner: string) => {
                                     )}>
                                     -1
                                 </button>
-
                                 <button 
                                     className="btn btn-secondary btn-md btn-circle m-2"
                                     onClick = {() => setPlayerPoints(
@@ -91,6 +99,7 @@ const gameOver = (winner: string) => {
                                     +1
                                 </button>
                             </div>
+
                             <div className="flex gap-3 items-center mb-5"
                             >
                                 <h3>Trotter, Razorback: 5 points each</h3>
@@ -104,7 +113,6 @@ const gameOver = (winner: string) => {
                                     )}>
                                     -5
                                 </button>
-
                                 <button 
                                     className="btn btn-secondary btn-md btn-circle m-2"
                                     onClick = {() => setPlayerPoints(
@@ -116,6 +124,7 @@ const gameOver = (winner: string) => {
                                     +5
                                 </button>
                             </div>
+
                             <div className="flex gap-3 items-center mb-5"
                             >
                                 <h3>Snouter: 10 points each</h3>
@@ -129,7 +138,6 @@ const gameOver = (winner: string) => {
                                     )}>
                                     -10
                                 </button>
-
                                 <button 
                                     className="btn btn-secondary btn-md btn-circle m-2"
                                     onClick = {() => setPlayerPoints(
@@ -140,6 +148,123 @@ const gameOver = (winner: string) => {
                                     )}>
                                     +10
                                 </button>
+                            </div>
+
+                            <div className="flex gap-3 items-center mb-5"
+                            >
+                                <h3>Leaning Jowler: 15 points each</h3>
+                                <button 
+                                    className="btn btn-neutral btn-md btn-circle m-2"
+                                    onClick = {() => setPlayerPoints(
+                                        playerPoints.map (y => [
+                                            y[0]
+                                            , y[0]===x ? y[1] -15 : y[1]
+                                        ])
+                                    )}>
+                                    -15
+                                </button>
+                                <button 
+                                    className="btn btn-secondary btn-md btn-circle m-2"
+                                    onClick = {() => setPlayerPoints(
+                                        playerPoints.map (y => [
+                                            y[0]
+                                            , y[0]===x ? y[1] +15 : y[1]
+                                        ])
+                                    )}>
+                                    +15
+                                </button>
+                            </div>
+
+                            <div className="flex gap-3 items-center mb-5"
+                            >
+                                <h3>Double Trotter, Double Razorback: 20 points</h3>
+                                <button 
+                                    className="btn btn-neutral btn-md btn-circle m-2"
+                                    onClick = {() => setPlayerPoints(
+                                        playerPoints.map (y => [
+                                            y[0]
+                                            , y[0]===x ? y[1] -20 : y[1]
+                                        ])
+                                    )}>
+                                    -20
+                                </button>
+                                <button 
+                                    className="btn btn-secondary btn-md btn-circle m-2"
+                                    onClick = {() => setPlayerPoints(
+                                        playerPoints.map (y => [
+                                            y[0]
+                                            , y[0]===x ? y[1] +20 : y[1]
+                                        ])
+                                    )}>
+                                    +20
+                                </button>
+                            </div>
+
+                            <div className="flex gap-3 items-center mb-5"
+                            >
+                                <h3>Double Snouter: 40 points</h3>
+                                <button 
+                                    className="btn btn-neutral btn-md btn-circle m-2"
+                                    onClick = {() => setPlayerPoints(
+                                        playerPoints.map (y => [
+                                            y[0]
+                                            , y[0]===x ? y[1] -40 : y[1]
+                                        ])
+                                    )}>
+                                    -40
+                                </button>
+                                <button 
+                                    className="btn btn-secondary btn-md btn-circle m-2"
+                                    onClick = {() => setPlayerPoints(
+                                        playerPoints.map (y => [
+                                            y[0]
+                                            , y[0]===x ? y[1] +40 : y[1]
+                                        ])
+                                    )}>
+                                    +40
+                                </button>
+                            </div>
+
+                            <div className="flex gap-3 items-center mb-5"
+                            >
+                                <h3>Double Leaning Jowler: 60 points</h3>
+                                <button 
+                                    className="btn btn-neutral btn-md btn-circle m-2"
+                                    onClick = {() => setPlayerPoints(
+                                        playerPoints.map (y => [
+                                            y[0]
+                                            , y[0]===x ? y[1] -60 : y[1]
+                                        ])
+                                    )}>
+                                    -60
+                                </button>
+                                <button 
+                                    className="btn btn-secondary btn-md btn-circle m-2"
+                                    onClick = {() => setPlayerPoints(
+                                        playerPoints.map (y => [
+                                            y[0]
+                                            , y[0]===x ? y[1] +60 : y[1]
+                                        ])
+                                    )}>
+                                    +60
+                                </button>
+                            </div>
+                            <div className="flex gap-3 items-center mb-5"
+                            >
+                                <button className="btn btn-md btn-oval btn-warning">
+                                    Pig Out! 
+                                </button>
+                                <p>Lose all points this turn.</p>
+                                <p>placeholder button - need to figure out how to track points per turn separately from points per game. Also how to track in fun facts</p>
+                            </div>
+
+                            <div className="flex gap-3 items-center mb-5"
+                            >
+                                <button className="btn btn-md btn-oval btn-warning">
+                                    OINKER!! 
+                                </button>
+                                <p>Lose all points in game!</p>
+                                <p>placeholder button - need to figure out how to track this in fun facts. Also set it up to erase all points.</p>
                             </div>
                                 <button
                                     key={x}
